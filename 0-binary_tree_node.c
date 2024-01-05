936d9d7976748ad9805715d7a3f84de0937de2b7
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-01-05 17:17:26 -0400

    fixing

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,16 +11,19 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+	binary_tree_t *newnode;
 
-	new = malloc(sizeof(binary_tree_t));
-	if (new == NULL)
+	(void) (*newnode);
+	(void) (value);
+
+	newnode = malloc(sizeof(binary_tree_t));
+	if (newnode == NULL)
 		return (NULL);
 
-	new->n = value;
-	new->parent = parent;
-	new->left = NULL;
-	new->right = NULL;
+	newnode->n = value;
+	newnode->parent = parent;
+	newnode->left = NULL;
+	newnode->right = NULL;
 
-	return (new);
+	return (newnode);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,11 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -9,6 +14,7 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
+
 struct binary_tree_s
 {
     int n;
@@ -24,6 +30,23 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 
 typedef struct binary_tree_s heap_t;
+
+/**
+ * struct queve - simple queue
+ * @node: is the value
+ * @next: is the next node in the queue
+ */
+
+struct queue
+{
+	binary_tree_t *node;
+	struct queue *next;
+};
+
+typedef struct queue queue;
+
+void binary_tree_print(const binary_tree_t *);
+
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
