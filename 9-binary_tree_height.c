0ec74c9ee7d99d723b052442d0e8e2a400e5d0f5
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-01-09 14:54:43 -0400

    fixing task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -19,5 +19,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	right_height = binary_tree_height(tree->right);
 
-	return ((left_height > right_height ? left_height : right_height) + 1);
+	return ((left_height > right_height) ? (left_height + 1) : (right_height + 1));
 }
