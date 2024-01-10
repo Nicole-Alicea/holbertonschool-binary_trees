98118c926061a6b90bc398c63593742faf7bef4e
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-01-09 23:19:19 -0400

    fixing task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -32,19 +32,18 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	size_t right_height = 0, left_height = 0;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	if (tree->left != NULL)
-	{
-		left_height = binary_tree_height(tree->left);
-	}
+
 	if (tree->right != NULL)
-	{
-		right_height = binary_tree_height(tree->right);
-	}
-	return (left_height - right_height);
+	right_h = binary_tree_height(tree->right);
+
+	if (tree->left != NULL)
+	left_h = binary_tree_height(tree->left);
+
+	return (left_h - right_h);
 }
