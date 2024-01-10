00bf2a4aef99a3c7f771b5b25c27d955c02b917a
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-01-09 22:26:59 -0400

    fixing task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -24,10 +24,10 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 
 		if (left_depth > right_depth)
 		{
-			return (left_depth + 1);
+			return (left_depth);
 		}
 		else
 		{
-			return (right_depth + 1);
+			return (right_depth);
 		}
 }
