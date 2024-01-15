4048ad399488ef8d6d9428e157c0113764f73fc9
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-01-14 21:42:51 -0400

    task 21

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -7,45 +7,30 @@
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	binary_tree_t *current,
+	const binary_tree_t *current,
 		      **queue = malloc(sizeof(binary_tree_t *) * 1000);
 	int front = 0, rear = 0, flag = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	queue[rear++] = (binary_tree_t *)tree;
+	queue[rear++] = tree;
 
 	while (front < rear)
 	{
 		current = queue[front++];
 		
-		if (current->left == NULL)
+		if ((current->left == NULL && (flag = 1)) || (flag && free(queue)))
 		{
-			flag = 1;
-		}
-		else if (flag)
-		{
-			free(queue);
 			return (0);
 		}
-		else
+		queue[rear++] = current->left;
+
+		if ((current->right == NULL && (flag = 1)) || (flag && free(queue)))
 		{
-			queue[rear++] = current->left;
-		}
-		if (current->right == NULL)
-		{
-			flag = 1;
-		}
-		else if (flag)
-		{
-			free(queue);
 			return (0);
 		}
-		else
-		{
-			queue[rear++] = current->right;
-		}
+		queue[rear++] = current->right;
 	}
 	free(queue);
 	return (1);
